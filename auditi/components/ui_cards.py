d28e8cbf27e86d5c15feabb040c7b270943e622a
Controller <controller.rem@gmail.com>
2026-03-15 19:38:26 -0300

    Delete auditi directory

--- auditi/components/ui_cards.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def metric_card(title: str, value, delta=None):
-    st.metric(label=title, value=value, delta=delta)
-
-def section_title(title: str):
-    st.markdown(f"### {title}")
--- auditi/services/dashboard_service.py
+++
@@ -1,9 +0,0 @@
-from modules import faturamento, estoque, pedidos, financeiro
-
-def load_dashboard_data():
-    return {
-        "faturamento": faturamento.get_data(),
-        "estoque": estoque.get_data(),
-        "pedidos": pedidos.get_data(),
-        "financeiro": financeiro.get_data(),
-    }
